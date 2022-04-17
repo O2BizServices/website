1f628c15dd9a282fbc3c4ab7350f2f085645a5de
chandan mhatre <31874891+chandanmhatre@users.noreply.github.com>
2022-04-17 16:29:29 +0530

    ok

--- o2crusher/dev/flutter_service_worker.js
+++ o2crusher/dev/flutter_service_worker.js
@@ -30,9 +30,9 @@ const RESOURCES = {
 "favicon.ico": "9c08cb6ac0c3ef75b4ad5931affb3103",
 "icons/Icon-192.png": "986d72640aaab18f1b1dc3d481d0244a",
 "icons/Icon-512.png": "c4ecb54ab43a7362b647c5ee00cddaaa",
-"index.html": "dec7071fddd068ba2ae0b26de7038d9a",
-"/": "dec7071fddd068ba2ae0b26de7038d9a",
-"main.dart.js": "0bb5b12d6a6989e348b5865ebf6d3522",
+"index.html": "71ade52fd7cdfe2a058d1b60ecc6d83b",
+"/": "71ade52fd7cdfe2a058d1b60ecc6d83b",
+"main.dart.js": "e2b8f792a048b1adc28118324a0f2d8a",
 "manifest.json": "368ebefc42a091127131e5ea17ee6e5e",
 "version.json": "9cdf811eb10f8f8bed9c68ab69c9065c"
 };
