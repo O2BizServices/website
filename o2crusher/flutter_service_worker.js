4a94cb94db9f196cd117423d62b853f10d5a8e0e
Vikrant3d <vikrantthakur15@gmail.com>
2022-05-05 17:23:27 +0530

    Changes

--- o2crusher/flutter_service_worker.js
+++ o2crusher/flutter_service_worker.js
@@ -21,7 +21,7 @@ const RESOURCES = {
 "assets/assets/images/stock/top.png": "5828a01cb6f04ba4a309df8bb6c06f13",
 "assets/FontManifest.json": "549d4f05705b6e948f5154c30a31bf5c",
 "assets/fonts/MaterialIcons-Regular.otf": "7e7a6cccddf6d7b20012a548461d5d81",
-"assets/NOTICES": "566f6d0e56d3e8e5d08d21d28a8840ab",
+"assets/NOTICES": "138e488ec226bf04ae946f739435b780",
 "assets/packages/cupertino_icons/assets/CupertinoIcons.ttf": "6d342eb68f170c97609e9da345464e5e",
 "assets/packages/fluttertoast/assets/toastify.css": "a85675050054f179444bc5ad70ffc635",
 "assets/packages/fluttertoast/assets/toastify.js": "e7006a0a033d834ef9414d48db3be6fc",
@@ -35,11 +35,11 @@ const RESOURCES = {
 "favicon.ico": "9c08cb6ac0c3ef75b4ad5931affb3103",
 "icons/Icon-192.png": "986d72640aaab18f1b1dc3d481d0244a",
 "icons/Icon-512.png": "c4ecb54ab43a7362b647c5ee00cddaaa",
-"index.html": "c8c3dbcf48602b26a9f3c3d0d8bf4085",
-"/": "c8c3dbcf48602b26a9f3c3d0d8bf4085",
-"main.dart.js": "f05da7bbadb519f77490cc2aa5a6094d",
+"index.html": "fcbdbc5823181668e9d956ad9bfea713",
+"/": "fcbdbc5823181668e9d956ad9bfea713",
+"main.dart.js": "c40d5df09cb8fe18b5a8a77e452a40b5",
 "manifest.json": "368ebefc42a091127131e5ea17ee6e5e",
-"version.json": "b6e0073b705c423a7bdbf354f80cffbb"
+"version.json": "3d339ae4565f3c206ec7b9226e91a5e6"
 };
 
 // The application shell files that are downloaded before a service worker can
